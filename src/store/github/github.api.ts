bd698fbc19195749c2e4904f7ae5915f775e30a3
nikitakukharchuk <nikitakukharchuk@mail.ru>
2022-07-15 16:14:13 +0300

    init (search, dropdown) : danteproject

---
+++ src/store/github/github.api.ts
@@ -0,0 +1,26 @@
+import { build } from '@reduxjs/toolkit/dist/query/core/buildMiddleware/cacheLifecycle'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { IUser, ServerResponse } from '../../models/models'
+
+export const githubApi = createApi({
+    reducerPath: 'github/api',
+    baseQuery: fetchBaseQuery ({
+        baseUrl:'https://api.github.com/'
+    }),
+    endpoints: build => ({
+        searchUsers: build.query<IUser[], string>({
+            query: (search: string) => ({
+                url: `search/users`,
+                params: {
+                    q: search,
+                    per_page: 10
+                }
+            }),
+            transformResponse: (response: ServerResponse<IUser>) => response.items 
+                
+            
+        })
+    })
+})
+
+export const {useSearchUsersQuery} = githubApi
